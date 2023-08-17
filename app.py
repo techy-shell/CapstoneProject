a2d545618422885cbdd609fe9963c39978def361
Rachelle Maconeghy <rnmaconeghy@outlook.com>
2023-08-17 15:28:37 +0100

    Commiting added files

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+    return f'Hello Udacity, my name is {escape(name)}!'
